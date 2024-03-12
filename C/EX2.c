9c6237e89d3fbc7d39d5e574b2fccb25132b2163
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-03-12 14:13:35 -0300

    Update EX2.c

--- C/EX2.c
+++ C/EX2.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-main(){
+int main(){
     float n;
     printf("Choose one number for convert to fahrenheit: ");
     scanf("%f", &n);
-    
+    float f = n*1.8 +32;
+    printf("O valor em Farenheits: %f\n",f);
+    float k = (n+459.67)*5/9;
+    printf("O valor em Kelvin :%f\n ", k); 
 
+    
 }
