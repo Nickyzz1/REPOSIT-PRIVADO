378a2eb11a3c4d02604f5a7b24b788be1599aa87
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-03-13 14:42:01 -0300

    Create ANOTES.C

---
+++ C/ANOTES.C
@@ -0,0 +1,30 @@
+//ponteiro aponta o arquivo FILE *arquivo
+//SEMPRE que vc der fopen, sempre de fclose
+
+//ex: código paraa abrir um txt em #C
+//condicionais--> condição, num labirinto vc vai para onde tem luz; isso é condicional
+
+
+//#include <stdio.h>
+//int main(){
+    //FILE * arquivo;
+    //char nome[50];
+    //prinf("Digite o nome do arquivo TXT: ");
+    //scanf("%s", &nome);
+
+    //arquivo = fopen(nome, "w");
+    //fprint(arquivo,"%s", "ola");
+    //fclose(arquivo);
+
+    //return 0;
+}
+
+//operador ternário
+
+//int a =10
+//int b = 20
+//int c;
+//c = (a < b)? a:b
+//orintf("%d", c)
+
+//sempre que fizer um sweet coloque um break ou ele não irá parar, irá ler mais do que precisa  e dará erro
