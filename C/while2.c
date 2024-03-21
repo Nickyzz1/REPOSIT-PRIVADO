5f605c6a6a7b7be6bc133e1b9a1a7649d05ab868
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-03-21 16:08:45 -0300

    Update while2.c

--- C/while2.c
+++ C/while2.c
@@ -2,17 +2,28 @@
 
 int main(){
     int max = 60;
+    int choose = 0;
     int value=0;
-    while(value < max){
-        printf("Enter the amount you want to kit:  ");
-        scanf("%i", &value);
-        if (value < max){
-            printf("You cannot withdraw this amount!\n\n");
+    int fla =1;
+    while(fla==1 ){
+        int saldo;
+        printf("Enter the number about what do you want access:\n1-Withdraw\n2-Deposit\n");
+        scanf("%i", &choose);
+        if(choose ==2){
+            printf("Enter the amount you want to Deposit:  ");
+            scanf("%i", &value);
+            saldo = saldo + value;
         }
-        else{
+        if(choose==1){
+            printf("Enter de value thats you want kit: ");
+            scanf("%i", &value);
+            if (value < max || value >saldo ){
+                printf("You cannot withdraw this amount!\n\n");
+            }
+            if(value>=max && value<=saldo){
 
-            printf("The value %i was whithdraw succesfully!\n", value);
-        }
+                printf("The value %i was whithdraw succesfully!\n", value);
+            }}}
+        return 0; 
     }
-    return 0;
-}
+    
