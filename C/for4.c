5394916b9e1921b3578d44dfc412e1433d079c08
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-03-22 14:23:34 -0300

    Update for4.c

--- C/for4.c
+++ C/for4.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    int num;
+    int num, count =0, i =0;
 
     printf("ENTER A NUMBER FOR DISCOVER PRIME NUMBERS: ");
     scanf("%i", &num);
-    for(int i = 1; i <num+1; i++)
+    for(int j = 1; j <num+1; j++) #começa em 1e  vai até op num,de 1 em 1
     {
-       for(int j = 1; j<num+1; j++)
-       {
-            if(i%j!=0)
-            {
-                
-
+        count =0; #defini o count como zero poisn ele contará quantas vezes a divisãod e cada número deus zero
+        for(int i = 1; i<num+1; i++)
+        {
+                if(j % i ==0)
+                {
+                count++;
+                }
             }
-       }
-        printf("%i",i);
-    }
-
+            if(count==2) #se forem duas, 1 e ele mesmo, significa ser primo
+            {
+                printf("%i\n", j);
+            }
+        }  
 }
