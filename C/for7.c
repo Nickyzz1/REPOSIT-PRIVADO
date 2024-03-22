bdf70a26e6bde2498dba94afc2e195bd42cc73ef
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-03-22 20:41:42 -0300

    Update for7.c

--- C/for7.c
+++ C/for7.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
-int main(){
+
+int main() {
     int num;
-    int count=0;
+    int count = 0;
+    int fla = 1;
 
-    printf("Enter a number for discover it: ");
+    printf("Enter a number to discover it: ");
     scanf("%i", &num);
-    for(int i =1;i<=2;i++)
-    {
-        num = num/10;
-        while(num!=0){
-            count +=num;
+
+    for (int i = 1; i <= 255; i++) {
+        while (fla == 1) {
+            while (num != 0) {
+                //num = num / 10;Esta linha divide o valor atual de num por 10 e atribui o resultado de volta a num. Isso basicamente remove o último dígito do número. Por exemplo, se num for 123, após esta operação, num será 12
+                count += 1;
+            }
+            fla = 0;
         }
-        
     }
-    printf("%i",count);
 
+    printf("Number of digits: %d\n", count);
+
+    return 0;
 }
-//     do{
-//         num = num / 10;
-//         count ++;
-
-//     }while (num != 0);
-
-//     // for(;num > 0;num/10){
-//     //     count ++;
-//     // };
-//     printf(" %i ",count);
-
-// }
