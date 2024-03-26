c99713fe51a056c5528bbd317ffe01f23f3cc736
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-03-26 17:18:15 -0300

    Create calculador_logico.c

---
+++ C/calculador_logico.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#ifndef FUNCTION_C.H
+#define FUNCTION_C.H
+//and,not, nand, or, nor, xor, nxor
+
+int Not(int a,int b){
+    return ~a;
+}
+
+int And(int a, int b){
+        return a&b; 
+}
+
+int Nand(int a, int b){
+    return ~And(a,b);
+}
+
+int Or(int )
+
+
+#endif // !FUNTION_C.h
+
+
+
