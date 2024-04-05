989d30eb63894dd6946213c4fa6c9b68fa492c76
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-04-05 15:38:09 -0300

    Update notas.c

--- C/notas.c
+++ C/notas.c
@@ -1,1 +1,9 @@
+// vetor = array(mesmo nome)--> aramzena valores um seguido do outro.
 
+// tipo_do_elemento nome_do_vetor[tamanho];
+  
+// uma array tem o poder de armazenar valores do mesmo tipo um seguido do outro
+// como seria uma lista em C:
+// int vetor[5] = 1,2,3,4,5
+// string = sequencia de cxaractres em casinhas juntas
+//string é uma array/lista/vetor --> pois é uma sequência de caracteres
