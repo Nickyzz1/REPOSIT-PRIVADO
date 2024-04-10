c9c3c56da2f171ab9fa516c0280369779faaa8d1
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-04-10 17:08:18 -0300

    Update ex11.c

--- C/ex11.c
+++ C/ex11.c
@@ -2,7 +2,7 @@
 
 void invert(int * array, int len){
     for(int i =0; i<(len)/2; i++){
-        int temp = array[i];
+        int temp = array[i]; //-1 poois o tamanho vai até 6 e o indice vai só até o 5(0,1,2,3,4,5), - ipara pegar os dois números quie devem trocar, por exemplo 1 e 6, 2 e 5, 3 e 4;
         array[i] = array[len-i-1];
         array[len-i-1]= temp;
     }
