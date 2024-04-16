685b708b5639c94d3a707e54c5b4bc61477a1c3b
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-04-16 16:56:08 -0300

    Update jogo_da_velha.c

--- C/jogo_da_velha.c
+++ C/jogo_da_velha.c
@@ -9,8 +9,32 @@ char matriz[5][5] =
         {'7', -77, '8', -77,'9'}
     };
 
-void sub_x(){
+// void sub_x(){
+//     char index;
+//     printf("Player 1(x): Enter a number for play: \n");
+//     scanf("%c", &index);
+//     for(int i =0; i<5;i++){
+//         for(int j =0; j<5;j++){
+//             if(matriz[j][i] == index){
+//                 matriz[j][i] = 'X';
+//             }
+//         }
+//     }
+//     sub_o();
+    
+// }
+
+int main(){
+
     char index;
+    int fla =1;
+    while(fla == 1){
+        for(int i =0; i<5;i++){
+            for(int j =0; j<5;j++){
+                printf("%c", matriz[i][j]);
+            }
+            printf("\n");
+        }
     printf("Player 1(x): Enter a number for play: \n");
     scanf("%c", &index);
     for(int i =0; i<5;i++){
@@ -20,12 +44,7 @@ void sub_x(){
             }
         }
     }
-    sub_o();
-    
-}
-
-void sub_o(){
-    char index;
+    }
     printf("Player 2(o): Enter a number for play: \n");
     scanf("%c", &index);
     for(int i =0; i<5;i++){
@@ -34,22 +53,5 @@ void sub_o(){
                 matriz[j][i] = 'O';
             }
         }
-    }
-    sub_x();
-}
-
-
-int main(){
-
-    int fla =1;
-    while(fla == 1){
-        for(int i =0; i<5;i++){
-            for(int j =0; j<5;j++){
-                printf("%c", matriz[i][j]);
-            }
-            printf("\n");
-        }
-        sub_x();
-        sub_x();
     }
 }
