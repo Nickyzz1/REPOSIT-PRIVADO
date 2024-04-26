94644450ac855b9b8cfe9ce41cc6345d5cffb315
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-04-26 10:34:55 -0300

    Create mallocmatrizz.c

---
+++ C/mallocmatrizz.c
@@ -0,0 +1,17 @@
+# include "header.h"
+
+int main(){
+    int lines;
+    int col;
+    printf("Enter lines: ");
+    scanf("%i", &lines );
+    printf("Enter columns: ");
+    scanf("%i", &col );
+    int **matriz = allocmemory(lines, col);
+    for(int i=0;i<lines;i++){
+        for(int j=0;j<col;j++){
+            printf("[%i],", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
