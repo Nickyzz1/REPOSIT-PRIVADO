86b204caa2b4c8d23865e633eeb850f4ef4fae6a
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-04-29 14:49:29 -0300

    Update ex1.java

--- Java/ex1.java
+++ Java/ex1.java
@@ -1,23 +1,60 @@
 // compressor
-public class Ex{
-    public static void main(String[] args){
-        int[] array = new int[] {
-            244, 232, 241,123,
-            232,064,111,140
-        };
-        int[] compact = Compact(array);
-        // int[] conc = Concatenar(array);
-        for(int i =0; i<array.length;i++){
-            System.out.printf("\n%d", compact[i]);
-        }
+public class Main {
+  public static void main(String[] args) {
+    int[] array = new int[] {
+        244, 232, 241, 123,
+        232, 64, 111, 140
+    };
+    int[] compact = Compact(array);
+    // int[] conc = Concatenar(array);
+    for (int i = 0; i < compact.length; i++) {
+      System.out.printf("\n%d", compact[i]);
     }
-    public static int[] Compact(int[] lista){
-        int[] list = new int[lista.length/2];
-        for(int i=0;i< list.length;i++){
-            list[i]= (lista[i*2] & 240) | (lista[i*2+1]>>4);
-        }
-        return list;
+    int[] descompact = Descompact(compact);
+    for (int i = 0; i < descompact.length; i++) {
+      System.out.printf("\n%d", descompact[i]);
     }
+  }
+
+  public static int[] Compact(int[] array) {
+    int[] array_comp = new int[array.length / 2];
+    for (int i = 0; i < array_comp.length; i++) {
+      array_comp[i] = (array[i * 2] & 240) | (array[i * 2 + 1] >> 4);
+    }
+    return array_comp;
+  }
+
+  public static int[] Descompact(int[] array_comp) {
+    int[] array_des = new int[array_comp.length * 2];
+    for (int i = 0; i < array_comp.length; i++) {
+      array_des[i * 2] = (array_comp[i] & 240);
+      array_des[i * 2 + 1] = (array_comp[i] & 15) << 4;
+    }
+    return array_des;
+  }
+
+
+
+// // compressor
+// public class Ex{
+//     public static void main(String[] args){
+//         int[] array = new int[] {
+//             244, 232, 241,123,
+//             232,064,111,140
+//         };
+//         int[] compact = Compact(array);
+//         // int[] conc = Concatenar(array);
+//         for(int i =0; i<array.length;i++){
+//             System.out.printf("\n%d", compact[i]);
+//         }
+//     }
+//     public static int[] Compact(int[] lista){
+//         int[] list = new int[lista.length/2];
+//         for(int i=0;i< list.length;i++){
+//             list[i]= (lista[i*2] & 240) | (lista[i*2+1]>>4);
+//         }
+//         return list;
+//     }
 
 
     // public static int[] Concatenar(int[] lista){
