53d925ffe03d3e2bc70be97c51aac113ec4c7d7f
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-04-30 17:14:30 -0300

    Update rpg.c

--- C/Outros/rpg.c
+++ C/Outros/rpg.c
@@ -13,3 +13,20 @@ int main()
     printf("\n\n");
 
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    char name[10];
+    int choose;
+    printf("Digite seu nome: ");
+    scanf("%s", &name);
+
+    printf("\nVamos começar entao...\nVoce acabou de acordar em um lugar estranho...O que voce faz?\n\n");
+    printf("1 - Explorar o local\n2 - Ficar no local\n\n");
+    
+
+}
