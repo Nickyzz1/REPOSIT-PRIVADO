f4d2763e0192cd2bab451d6407e24dd4bf93c42e
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-04-30 15:45:34 -0300

    Delete Exercícios Senai directory

--- Exercícios Senai/ex5.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    double array[] = {3.14, 2.71, 1.618, 0.577};
-}
--- Exercícios Senai/ex7.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    char array[][6] = {"Alice", "Bob", "Carol", "David"};
-    int len = sizeof(array)/sizeof(array[0]);
-    for(int i=0; i<len; i++){
-        printf("%s, ", array[i]);
-    }}
