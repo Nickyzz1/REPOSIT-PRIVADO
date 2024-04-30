7874087a7c81652014bb19e7a8914fa68b48100f
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-04-30 17:11:49 -0300

    Create rpg.c

---
+++ C/Outros/rpg.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    char name[10];
+    int choose;
+    printf("Digite seu nome: ");
+    scanf("%s", &name);
+
+    printf("\nVamos começar entao...\n");
+    printf("\n\n");
+
+}
