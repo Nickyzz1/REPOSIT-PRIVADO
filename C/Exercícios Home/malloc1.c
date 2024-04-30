1fb6c57531690a92fccfe3984379e2d8fd2dc6b7
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-04-30 16:16:59 -0300

    Update malloc1.c

--- C/Exercícios Home/malloc1.c
+++ C/Exercícios Home/malloc1.c
@@ -1,17 +1,24 @@
-#include <stdio.h> //faça uma função que receba duas matrizes e aloque memória para uma matriz resultado;
-//função que aloca memória para a matriz resultado e soma as matrizes;
-int alloc(int matriz1[][3], int matriz2[][3]){
-    int **matriz3= malloc(sizeof(int*)*3); //criando matriz resultado
-    for(int i = 0; i<3;i++){
+#include <stdio.h> 
+#include <stdlib.h>
+//faça uma função que receba duas matrizes e aloque memória para uma matriz resultado;
+
+int **alloc(int matriz1[][3], int matriz2[][3]) //função que aloca memória para a matriz resultado e soma as matrizes
+{
+    int **matriz3 = malloc(sizeof(int*)*3); //criando matriz resultado
+    for(int i = 0; i<3;i++)
+    {
         matriz3[i] = (int*)malloc(sizeof(int)*3);
     }
-    for(int j = 0; j<3;j++){ //somando matrizes num item
-        for(int k = 0; k<3;k++){
-            matriz3[j][k] = matriz1[j][k] + matriz2[j][k];
+    for(int j = 0; j<3;j++) //somando matrizes num item
+    { 
+        for(int k = 0; k<3;k++) 
+        {
+            matriz3[j][k] = matriz1[j][k] + matriz2[j][k]; 
         }
     }
-    return matriz3; 
+    return matriz3;
 }
+
 int main(){ 
     int matriz1[3][3] = {
     {1,2,3},
@@ -26,15 +33,16 @@ int main(){
     };
 
     int ** soma = alloc(matriz1,matriz2);
-    for(int j = 0; j<3;j++){
-        for(int k = 0; k<3;k++){
+    for(int j = 0; j<3;j++)
+    {
+        for(int k = 0; k<3;k++)
+        {
             printf("[%d]", soma[j][k]);
         }
         printf("%s", "\n");
     }
-}
-    // Liberando memória
-    for(int j = 0; j < 3; j++){
+    for(int j = 0; j < 3; j++) // Liberando memória
+    {
         free(soma[j]);
     }
     free(soma);
