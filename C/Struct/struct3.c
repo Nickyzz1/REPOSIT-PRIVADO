6b9a1b25688316d50a5e828b3db71dbb0e44f4d4
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-05-07 17:08:50 -0300

    Update struct3.c

--- C/Struct/struct3.c
+++ C/Struct/struct3.c
@@ -1,100 +1,80 @@
 #include <stdio.h>
 
-typedef struct{
+typedef struct {
     char name[10];
     int registration;
     int nota1;
     int nota2;
     int nota3;
+} Student;
 
-}Student;
+int main() {
+    // cria as variáveis usadas
+    Student student[5];
+    int meanBig = 0;
+    int meanLow = 10; // Inicializar com o maior valor possível
+    int meanStudent = 0;
+    char arrayAprovados[5][10];
+    char arrayReprovados[5][10];
+    int countAprovados = 0;
+    int countReprovados = 0;
 
-
-int main() 
-{
-    //cria as variáveis usadas
-    Student student[2];
-    int  temp;
-    int  meanBig;
-    int  meanLow;
-    int  meanStudent;
-    char arrayAprovados[20][20];
-    char arrayReprovados[20][20];
-
-    //cria os 5 alunos
-    for(int i = 0; i < 2; i++) 
-    {
-        printf("Enter a student %i: ", i );
-        scanf("%s", &student[i].name);
-        printf("Enter the registrationt %i: ", i );
-        scanf("%d", &student[i].registration);
-        printf("Enter the grade for the first test %i: ", i );
-        scanf("%i", &student[i].nota1);
-        printf("Enter the grade for the second test %i: ", i );
-        scanf("%i", &student[i].nota2);
-        printf("Enter the grade for the third test %i: ", i );
-        scanf("%i", &student[i].nota3);
-
-        //guarda a nota iterada numa var temporária
-        temp = student[i].nota1;
-
-        //condição para mudar o valor de temp
-        if(student[i].nota1 > temp) 
-        {
-            temp = student[i].nota1;
-        }
-
-        //atribuição dos calculos das médias
-        meanBig = ((student[i].nota1) * (student[i].nota2 )* (student[i].nota3))/3;
-        meanLow = (student[i].nota1 * student[i].nota2 * student[i].nota3)/3;
-        meanStudent = (student[i].nota1 * student[i].nota1 * student[i].nota1)/3;
-
-        //comparação para mudar o valor do meanBig
-        if(meanBig < (student[i].nota1 * student[i].nota2 * student[i].nota3)/3) 
-        {
-            meanBig = (student[i].nota1 * student[i].nota2 * student[i].nota3)/3;
-        }
+    // cria os 2 alunos
+    for (int i = 0; i < 2; i++) {
         
-        //comparação para mudar o valor do meanLow
-        if(meanLow > (student[i].nota1 * student[i].nota2 * student[i].nota3)/3) 
-        {
-            meanLow = (student[i].nota1 * student[i].nota2 * student[i].nota3)/3;
+        printf("Enter a student %i: ", i);
+        scanf("%s", student[i].name);
+        printf("Enter the registration %i: ", i);
+        scanf("%d", &student[i].registration);
+        printf("Enter the grade for the first test %i: ", i);
+        scanf("%d", &student[i].nota1);
+        printf("Enter the grade for the second test %i: ", i);
+        scanf("%d", &student[i].nota2);
+        printf("Enter the grade for the third test %i: ", i);
+        scanf("%d", &student[i].nota3);
+
+        // Calcula a média do estudante
+        meanStudent = (student[i].nota1 + student[i].nota2 + student[i].nota3) / 3;
+
+        // Calcula a média mais alta
+        if (meanStudent > meanBig) {
+            meanBig = meanStudent;
         }
 
-        //comparação para reprovar e aprovar
-        if(meanStudent >= 6) 
-        {
-            //adiciona a lista aprovados
-            for(int j = 0; j < 2; j++) 
-            {
-                arrayAprovados[i][j] = student[i].name[j];
+        // Calcula a média mais baixa
+        if (meanStudent < meanLow) {
+            meanLow = meanStudent;
+        }
+
+        // Verifica se o aluno foi aprovado ou reprovado
+        if (meanStudent >= 6) {
+            // Adiciona à lista de aprovados
+            for (int j = 0; student[i].name[j] != '\0'; j++) {
+                arrayAprovados[countAprovados][j] = student[i].name[j];
             }
-        } 
-        else 
-        {
-            //adiciona a lista reprovados
-            for(int j = 0; j < 2; j++) 
-            {
-                arrayReprovados[i][j] = student[i].name[j];
+            countAprovados++;
+        } else {
+            // Adiciona à lista de reprovados
+            for (int j = 0; student[i].name[j] != '\0'; j++) {
+                arrayReprovados[countReprovados][j] = student[i].name[j];
             }
-        } 
+            countReprovados++;
+        }
     }
 
-    //mostra os resultados
-    printf("\n\nThe biggest grade student on test 1: %i", temp);
-    printf("\n\nThe biggest mean: %i", meanBig);
-    printf("\n\nThe Lowest mean: %i", meanLow);
+    // mostra os resultados
+    printf("\n\nThe biggest mean: %d", meanBig);
+    printf("\n\nThe Lowest mean: %d", meanLow);
 
-    printf("\n\nAproved:");
-    for(int i = 0; i < 2; i++) 
-    {
-        printf("%s\n" , arrayAprovados[i]);
+    printf("\n\nAproved:\n");
+    for (int i = 0; i < countAprovados; i++) {
+        printf("%s\n", arrayAprovados[i]);
     }
 
-    printf("\n\nReproved:");
-    for(int i = 0; i < 2; i++) 
-    {
-        printf("%s" , arrayReprovados[i]);
+    printf("\n\nReproved:\n");
+    for (int i = 0; i < countReprovados; i++) {
+        printf("%s\n", arrayReprovados[i]);
     }
 
+    return 0;
 }
