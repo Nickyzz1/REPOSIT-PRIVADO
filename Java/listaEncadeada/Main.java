83c4fa3f97c86fb47c4cb765be35647566eaa6c3
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-05-10 16:54:04 -0300

    Create Main.java

---
+++ Java/listaEncadeada/Main.java
@@ -0,0 +1,9 @@
+//o primeiro elem4ento na verdade é o ultimo adc, pois está no topoda pilha
+//por sua vez, po último elemento na pilha é o primeiro a sair pois é o primerio adc
+public class Main {
+    public static void main(String[] args) {
+        // linkedList() é um construtor
+        linkedList lista = new linkedList();
+        System.out.println(lista.getTamanho());
+    }
+}
