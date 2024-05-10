4edef0c248fa2acadf2b3743918996aac8b00bc8
Nicky-dev-rs <160642381+Nickyzz1@users.noreply.github.com>
2024-05-10 09:31:05 -0300

    Add files via upload

---
+++ Java/Desafio 4 - Startup/ListaEmpregados.java
@@ -0,0 +1,31 @@
+class ListaEmpregados
+{
+    private NoEmpregados head = null;
+    private int size = 0;
+
+    void add(Empregado empregado)
+    {
+        size++;
+        NoEmpregados node = new NoEmpregados(empregado);
+        if (head == null) {
+            head = node;
+            return;
+        }
+
+        NoEmpregados atual = head;
+        while (atual.getNext() != null) {
+            atual = atual.getNext();
+        }
+
+        atual.setNext(node);
+    }
+
+    // Empregado get(int index)
+    // {
+
+    // }
+
+    int getSize() {
+        return size;
+    }
+}
