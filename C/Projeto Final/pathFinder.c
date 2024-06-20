f2a7053954b1fff5c88087bc74476e56c2efd6fb
Nicolle Rodrigues Siqueira <160642381+Nickyzz1@users.noreply.github.com>
2024-06-20 16:53:36 -0300

    Update pathFinder.c

--- C/Projeto Final/pathFinder.c
+++ C/Projeto Final/pathFinder.c
@@ -1,251 +1,128 @@
-#include <stdio.h> // rand é da stdlib
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <time.h>
-#define MAX_NUMBERS 143
 
-//thats define poisitions for two numbers, the start and the destany
-void generatePosition(int **matriz)
-{
-    srand(time(NULL));
-    int YStart = rand() % 13;
-    int XStart = rand() % 13;
-    int YFinal = rand() % 13;
-    int XFinal = rand() % 13;
+#define INF 99999 //valor infinito para comparações de distancias
+#define MAX_NODES 100
 
-    matriz[XStart][YStart] = 9;
-    matriz[XFinal][YFinal] = 10;
-    
-    printf("\nstart col: %i, start line:  %i, final col: %i, final line %i", YStart, XStart, YFinal, XFinal);
+typedef struct {
+    int node;
+    int cost;
+} Edge;
+
+typedef struct {
+    Edge edges[MAX_NODES]; // Lista de arestas
+    int num_edges;         // Número de arestas saindo deste nó
+} GraphNode;
+
+typedef struct {
+    GraphNode nodes[MAX_NODES]; // Lista de nós do grafo
+    int num_nodes;              // Número de nós no grafo
+} Graph;
+
+// Função para inicializar o grafo
+void initialize_graph(Graph *graph, int num_nodes) {
+    graph->num_nodes = num_nodes;
+    for (int i = 0; i < num_nodes; ++i) {
+        graph->nodes[i].num_edges = 0;
+    }
 }
 
-void generateArena(int **matriz)
-{
-    int position;
-    int count;
+// Função para adicionar uma aresta direcionada no grafo
+void add_edge(Graph *graph, int from, int to, int cost) {
+    int num_edges = graph->nodes[from].num_edges;
+    graph->nodes[from].edges[num_edges].node = to;
+    graph->nodes[from].edges[num_edges].cost = cost;
+    graph->nodes[from].num_edges++;
+}
 
-    for(int i = 0; i < 12; i++)
-    {
-        for(int j = 0; j < 12; j++)
-        {
-            if(matriz[i][j] != 9 && matriz[i][j] != 10)
-            {
-                position = rand() % 2;
-                matriz[i][j] = position;
-                count += 1;
+// Algoritmo A* para encontrar o caminho mais curto de start para goal
+void a_star(Graph *graph, int start, int goal) {
+    // Vetor de distâncias mínimas estimadas (heurísticas) do nó inicial até cada nó
+    int heuristic[MAX_NODES];
+    for (int i = 0; i < graph->num_nodes; ++i) {
+        heuristic[i] = INF; // Inicializa todas as distâncias como infinito
+    }
+    heuristic[start] = 0; // A distância do nó inicial até ele mesmo é zero
+    
+    // Vetor de custo total do caminho do nó inicial até cada nó
+    int cost_so_far[MAX_NODES];
+    for (int i = 0; i < graph->num_nodes; ++i) {
+        cost_so_far[i] = INF; // Inicializa todos os custos como infinito
+    }
+    cost_so_far[start] = 0; // O custo do caminho do nó inicial até ele mesmo é zero
+    
+    // Estrutura para armazenar os nós a serem explorados
+    int open_set[MAX_NODES];
+    int open_set_size = 1;
+    open_set[0] = start;
+    
+    // Vetor de nós visitados
+    bool visited[MAX_NODES] = {false};
+    
+    while (open_set_size > 0) {
+        // Encontra o nó no open set com o menor custo estimado (heurístico)
+        int current = open_set[0];
+        int current_index = 0;
+        for (int i = 1; i < open_set_size; ++i) {
+            if (heuristic[open_set[i]] < heuristic[current]) {
+                current = open_set[i];
+                current_index = i;
+            }
+        }
+        
+        // Remove o nó atual do open set
+        open_set[current_index] = open_set[open_set_size - 1];
+        open_set_size--;
+        
+        // Se o nó atual é o goal, terminamos
+        if (current == goal) {
+            break;
+        }
+        
+        // Marca o nó atual como visitado
+        visited[current] = true;
+        
+        // Percorre os vizinhos do nó atual
+        for (int i = 0; i < graph->nodes[current].num_edges; ++i) {
+            int neighbor = graph->nodes[current].edges[i].node;
+            int new_cost = cost_so_far[current] + graph->nodes[current].edges[i].cost;
+            
+            // Se o novo caminho encontrado até o vizinho é melhor
+            if (new_cost < cost_so_far[neighbor]) {
+                cost_so_far[neighbor] = new_cost;
+                heuristic[neighbor] = new_cost; // Aqui poderia ser uma heurística mais sofisticada
+                if (!visited[neighbor]) {
+                    open_set[open_set_size] = neighbor;
+                    open_set_size++;
+                }
             }
         }
     }
+    
+    // Exibe o caminho mais curto encontrado
+    printf("Caminho mais curto de %d para %d: %d\n", start, goal, cost_so_far[goal]);
 }
 
-void displayMatriz(int **matriz)
-{
-    printf("%s", "\n");
-    for(int i = 0; i < 12; i++)
-    {
-        for(int j = 0; j < 12; j++)
-        {
-            printf("%i, ",matriz[i][j]);
-        }
-        printf("%s", "\n");
-    }
+int main() {
+    Graph graph;
+    int num_nodes = 6;
+    initialize_graph(&graph, num_nodes);
+    
+    // Exemplo de adição de arestas no grafo
+    add_edge(&graph, 0, 1, 5);
+    add_edge(&graph, 0, 2, 10);
+    add_edge(&graph, 1, 3, 3);
+    add_edge(&graph, 2, 3, 1);
+    add_edge(&graph, 3, 4, 5);
+    add_edge(&graph, 2, 4, 8);
+    add_edge(&graph, 4, 5, 2);
+    
+    int start = 0;
+    int goal = 5;
+    
+    // Encontra o caminho mais curto usando o algoritmo A*
+    a_star(&graph, start, goal);
+    
+    return 0;
 }
-
-
-int main()
-{
-    int **matriz = malloc(sizeof(int)*12);
-    for(int i = 0; i < 12; i++)
-    {
-        matriz[i] = malloc(sizeof(int)*12);
-    }
-    // colocando valores iniciais poara a matriz
-    for(int i = 0; i < 12; i++)
-    {
-        for(int j = 0; j < 12; j++)
-        {
-            matriz[i][j] = 0;
-        }
-    }
-
-    generatePosition(matriz);
-    generateArena(matriz);
-    displayMatriz(matriz);
-
-    // li'berando memória
-    for(int i = 0; i < 12; i++)
-    {
-        free(matriz);
-        for(int j = 0; j < 12; j++)
-        {
-            free(matriz);
-        }
-    }
-
-    // {
-    //     {9,0,0,0,1,1,1,0,0,0,1,1},
-    //     {1,0,0,0,1,1,1,0,0,0,1,1},
-    //     {1,0,0,0,1,1,1,0,0,0,1,1},
-    //     {1,0,0,0,1,1,1,0,0,0,1,1},
-    //     {1,0,0,0,1,1,1,0,0,0,1,1},
-    //     {1,0,0,0,1,1,1,0,0,0,1,1},
-    //     {1,0,0,0,1,1,1,0,0,0,1,1},
-    //     {1,0,0,0,1,1,1,0,0,0,1,1},
-    //     {1,0,0,0,1,1,1,0,0,0,1,1},
-    //     {1,0,0,0,1,1,1,0,0,0,1,1},
-    //     {1,0,0,0,1,1,1,0,0,0,1,1},
-    //     {1,0,0,0,1,1,1,0,0,0,1,1}
-    // };
-
-//     for(int i = 0; i < 12; i++)
-//     {
-//         for(int j = 0; j < 12; j++)
-//         {
-//             free(matriz[i][j]);
-//         }
-//     }
-}
-
-
-
-
-// // Definição das constantes do jogo
-// #define BOARD_SIZE 10
-// #define MAX_ENEMIES 15
-
-// // Enumeração para as direções de movimento
-// typedef enum {
-//     UP,
-//     DOWN,
-//     LEFT,
-//     RIGHT
-// } Direction;
-
-// // Estrutura para representar o jogador e os inimigos
-// typedef struct {
-//     int x, y;  // Posição no tabuleiro
-// } Character;
-
-// // Função para inicializar o tabuleiro do jogo
-// void initialize_board(char board[BOARD_SIZE][BOARD_SIZE], Character *player, Character enemies[MAX_ENEMIES]) {
-//     // Preencher o tabuleiro com espaços vazios
-//     for (int i = 0; i < BOARD_SIZE; ++i) {
-//         for (int j = 0; j < BOARD_SIZE; ++j) {
-//             board[i][j] = ' ';
-//         }
-//     }
-
-//     // Posicionar o jogador no canto superior esquerdo
-//     player->x = 0;
-//     player->y = 0;
-//     board[player->x][player->y] = 'P';
-
-//     // Posicionar inimigos aleatoriamente no tabuleiro
-//     srand(time(NULL));
-//     for (int i = 0; i < MAX_ENEMIES; ++i) {
-//         enemies[i].x = rand() % BOARD_SIZE;
-//         enemies[i].y = rand() % BOARD_SIZE;
-//         board[enemies[i].x][enemies[i].y] = 'E';
-//     }
-// }
-
-// // Função para imprimir o tabuleiro
-// void print_board(char board[BOARD_SIZE][BOARD_SIZE]) {
-//     for (int i = 0; i < BOARD_SIZE; ++i) {
-//         for (int j = 0; j < BOARD_SIZE; ++j) {
-//             printf("%c ", board[i][j]);
-//         }
-//         printf("\n");
-//     }
-// }
-
-// // Função para mover o jogador
-// void move_player(Character *player, Direction dir) {
-//     switch (dir) {
-//         case UP:
-//             if (player->x > 0) player->x--;
-//             break;
-//         case DOWN:
-//             if (player->x < BOARD_SIZE - 1) player->x++;
-//             break;
-//         case LEFT:
-//             if (player->y > 0) player->y--;
-//             break;
-//         case RIGHT:
-//             if (player->y < BOARD_SIZE - 1) player->y++;
-//             break;
-//     }
-// }
-
-// // Função para mover um inimigo aleatoriamente
-// void move_enemy(Character *enemy) {
-//     Direction dir = rand() % 4;
-//     switch (dir) {
-//         case UP:
-//             if (enemy->x > 0) enemy->x--;
-//             break;
-//         case DOWN:
-//             if (enemy->x < BOARD_SIZE - 1) enemy->x++;
-//             break;
-//         case LEFT:
-//             if (enemy->y > 0) enemy->y--;
-//             break;
-//         case RIGHT:
-//             if (enemy->y < BOARD_SIZE - 1) enemy->y++;
-//             break;
-//     }
-// }
-
-// // Função principal do jogo
-// int main() {
-//     char board[BOARD_SIZE][BOARD_SIZE];
-//     Character player;
-//     Character enemies[MAX_ENEMIES];
-
-//     initialize_board(board, &player, enemies);
-
-//     // Loop principal do jogo
-//     while (true) {
-//         // Limpar a tela (para sistemas UNIX-like)
-//         system("clear");
-
-//         // Imprimir o tabuleiro
-//         print_board(board);
-
-//         // Ler o movimento do jogador
-//         printf("Digite o movimento (WASD para cima, baixo, esquerda, direita): ");
-//         char move;
-//         scanf(" %c", &move);
-
-//         // Movimentar o jogador
-//         switch (move) {
-//             case 'W':
-//                 move_player(&player, UP);
-//                 break;
-//             case 'S':
-//                 move_player(&player, DOWN);
-//                 break;
-//             case 'A':
-//                 move_player(&player, LEFT);
-//                 break;
-//             case 'D':
-//                 move_player(&player, RIGHT);
-//                 break;
-//         }
-
-//         // Movimentar os inimigos
-//         for (int i = 0; i < MAX_ENEMIES; ++i) {
-//             move_enemy(&enemies[i]);
-//         }
-
-//         // Verificar colisões e outras condições de jogo (não implementado neste exemplo)
-
-//         // Simular atraso para visualização (apenas para efeitos visuais no terminal)
-//         for (int i = 0; i < 100000000; ++i) {}
-
-//         // Fim de jogo (não implementado neste exemplo)
-
-//     }
-
-//     return 0;
-// }
