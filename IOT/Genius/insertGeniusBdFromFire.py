b71ebf3d197d1ac6db225183e2dd7dabdd008117
Nicolle Rodrigues Siqueira <160642381+Nickyzz1@users.noreply.github.com>
2024-06-24 10:55:20 -0300

    Update insertGeniusBdFromFire.py

--- IOT/Genius/insertGeniusBdFromFire.py
+++ IOT/Genius/insertGeniusBdFromFire.py
@@ -6,25 +6,27 @@ import json
 proxies = {'https': "http://disrct:etsps2024401@10.224.200.26:8080"}
 
 def InserirBD(sinal):
-    server = 'CA-C-0064P\SQLEXPRESS'
+    server = 'CA-C-0064P\\SQLEXPRESS'
     database = 'genius'
     cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';Trusted_Connection=yes')
     cursor = cnxn.cursor()
-    cursor.execute(f"INSERT tableGenius (score) VALUES ({sinal[0]})")
-    cursor.commit()
+    # Usando parâmetros para evitar SQL injection
+    cursor.execute("INSERT INTO tableGenius (player, score) VALUES (?, ?)", sinal[0], sinal[1])
+    cnxn.commit()
     print("Inserido com sucesso!")
 
 def apresentar(sinal):
-    print(f"score: {sinal[0]}")
+    print(f"Player: {sinal[0]}, Score: {sinal[1]}")
 
 while True:
     url = 'https://iiot-7276b-default-rtdb.firebaseio.com//Nicolle.json'
     cont = rq.get(url, proxies=proxies).content
     results = json.loads(cont)
+    Player = results.get("Player")
     Score = results.get("Score")
     if Score is not None:  # Verifica se Score não é None
-        print(f"score: {Score}")
-        valores = (Score,)  # Note a vírgula para fazer uma tupla de um único valor
+        print(f"Player: {Player} Score: {Score}")
+        valores = (Player, Score) #se a tupla for um valor é (valor,)
         apresentar(valores)
         InserirBD(valores)
         break
