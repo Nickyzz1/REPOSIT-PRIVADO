4c482318cb8d3072689d9ded14141bccc10e3329
Nicolle Rodrigues Siqueira <160642381+Nickyzz1@users.noreply.github.com>
2024-08-13 14:34:03 -0300

    Update base.sql

--- WEB/loginVmotion/assets/outros/base.sql
+++ WEB/loginVmotion/assets/outros/base.sql
@@ -3,9 +3,10 @@ use vmotion;
 
 create table cadastro(
 id int not null primary key identity,
-nome varchar(60) not null
+nome varchar(60) not null,
+senha varchar(60) not null
 );
 
-insert into cadastros (nome) values('nii');
+insert into cadastro (nome, senha) values('nii', '4566');
 
-select * from cadastros;
+select * from cadastro;
