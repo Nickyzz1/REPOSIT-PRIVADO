b60d08882284557e4453bac32c2a6727746f4423
Nicolle Rodrigues Siqueira <160642381+Nickyzz1@users.noreply.github.com>
2024-08-19 14:25:56 -0300

    Update cadastroAlunos.js

--- WEB/drud/v1/src/controllers/cadastroAlunos.js
+++ WEB/drud/v1/src/controllers/cadastroAlunos.js
@@ -1,4 +1,4 @@
-const aluno = require('../model/aluno');
+const aluno = require('../model/aluno'); // importa o script de criação de tabelas no swl server que está em aluno, que vai criar todas as tabelas para aluno
 // const aluno = require('../model/aluno');
 module.exports = {
 async aluno(req, res){
@@ -6,8 +6,10 @@ res.render('../views/cadastroAluno');
 },
 async alunoInsert(req, res){
 // Recebe as informações do front-end
+// pega todos os dados do body
 const dados = req.body;
 // Criando sala no banco de dados
+//define pelo name quais dados do body quer pegar
 await aluno.create({
 Nome: dados.nome,
 Idade: dados.idade,
@@ -15,6 +17,7 @@ Sexo: dados.sexo,
 Sala: dados.sala,
 Foto: dados.foto
 });
+// depois de inserir o user é redirecionado a página princiapal
 // Redirecionar para a página principal
 res.redirect('/');
 }
